4ba478c8b1261737eb130e7138faa56066f1d398
Oleksii Martynenko <oleksii.martynenko@nure.ua>
2021-05-20 19:40:03 +0300

    Created general structure of App (classes and pages) for MVVM pattern and StartPage

--- MobileApp/MobileApp/MobileApp/App.xaml.cs
+++ MobileApp/MobileApp/MobileApp/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MobileApp.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -10,7 +11,7 @@ namespace MobileApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new StartPage();
         }
 
         protected override void OnStart()
---
+++ MobileApp/MobileApp/MobileApp/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MobileApp.Models
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+        // Maybe usefull, idk
+        public override bool Equals(object obj)
+        {
+            User user = obj as User;
+            return this.Email.Equals(user.Email) && this.Password.Equals(user.Password);
+        }
+    }
+}
